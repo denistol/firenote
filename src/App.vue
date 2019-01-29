60778648e097124ae9b791e3cdc4d85f26e36eef
denistol <denistol@pm.me>
2019-01-29 23:37:10 +0200

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,84 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div id="app" class="row">
+    <div class="sidebar col">
+      <span @click="writeUserData(160)">item</span>
+      <span>item</span>
+      <span>item</span>
+      <span>item</span>
+      <span>item</span>
+      <span>item</span>
+      <span>item</span>
+      <span>item</span>
+      <span>item</span>
+      <span>item</span>
+      <span>item</span>
+      <span>item</span>
+      <span>item</span>
+      <span>item</span>
+      <span>item</span>
+    </div>
+    <div class="div content">
+      content
+    </div>
   </div>
 </template>
 
 <script>
+import firebase from 'firebase';
+import sha256 from 'sha256';
+import config from '../firebase_config.js'
+  // Initialize Firebase
+
+  firebase.initializeApp(config);
+
 export default {
-  name: 'App'
+  name: 'App',
+  methods:{
+    writeUserData:function(name) {
+      let randId = sha256( (Math.random()*9999).toString() );
+      firebase.database().ref('notes/'+randId).set({
+        username: name,
+      });
+    }
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+*{
+  font-family: sans-serif;
+  margin: 0;
+  padding: 0;
 }
+  #app{
+    height: 100vh;
+  }
+  div{
+    display: flex;
+    flex-direction: column;
+  }
+  .row{
+    flex-direction: row !important;
+  }
+  .col{
+    flex-direction: column !important;
+  }
+  .sidebar{
+    background-color: #eee;
+    width: 20%;
+    span{
+      border-bottom: 1px solid rgb(219, 219, 219);
+      cursor: pointer;
+      padding: 5px;
+      &:hover{
+        background-color: rgb(94, 94, 105);
+        color: #fff;
+      }
+    }
+  }
+  .content{
+    padding: 5px;
+    width: 80%;
+    flex: 1;
+  }
 </style>
